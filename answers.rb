294df951933891ac4518a7b3da6ad491dd239fdd
Bobby Caya <bob.caya1@gmail.com>
2018-02-09 15:19:22 -0700

    Add Answers class

---
+++ answers.rb
@@ -0,0 +1,17 @@
+class Answers
+  attr_accessor 
+
+  def initialize
+    @answers = ["Yes", "No", "Maybe","Outlook does not good", "Idk dude?", "It is a definite maybe", "Very doubtful"]
+  end 
+  def rand_answers
+    puts @answers.sample 
+  end 
+
+end 
+
+@answerss = Answers.new 
+
+@answerss.rand_answers 
+
+
