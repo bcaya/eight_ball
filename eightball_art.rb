a62a1e5c05ed9a4bb9a709315617f32d69c4b974
Shandelle Lovato <ShandelleLovato@gmail.com>
2018-02-10 15:11:59 -0700

    Adds better formatting for readability, colors because I like colors, ascii eight-ball Has easter egg section to add answers, but it's commented out because it doesn't work yet

---
+++ eightball_art.rb
@@ -0,0 +1,23 @@
+require 'colorize'
+@art = '
+                      ..+++MMMMMMMMMMMnn._
+                  _**###MMMMMMMMMMMMMMNNNNn\,
+                /MMMMMMM/            `78MMMNNn*.
+            //NMMMMMMMm      ..MMMM.     MMNNNnn!.
+          /nMMMMMMMM+=      :M     M.     7MMMMZZ!!!
+        ./nMMMMMMMMM=      .M.    MM.     <MMMMZZ!!!
+       ./nMMDMMMMMMM~      .nMMMMMMn.      .MMMMMZZ!!!
+      ./mmMOMMMMMMMM7      ?M     Mn      .<OMMMMZZ!!!
+      ./9MMMMMMMMMMMM      :nM:  .M.      .MMMMMMZZZ!!!
+      ./MMMMMMMMMMMMMM.      \nmnn/      .MMMMMMMMZZZ!!
+      ./MMMMMMMMMMMMMMM\               ..MMMMMMMMMMZ!!!
+       ?MMMMMMMMMMMMMMMMM#..        ./#MMMMMMMMMMMZZ!=
+        MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM!!!!
+        +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM!!!!
+          8MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM!!!!
+            DMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM!!!!!
+             :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM!!!!
+               `.MMMMMMMMMMMMMMMMMMMMMMMMM!!!!!
+                  ``.MMMMMMMMMMMMMMMMMMM!!!!!
+                      ```7MMMMMMMMM!!!!!/
+'
--- main.rb
+++ main.rb
@@ -9,20 +9,53 @@
 # - via easter egg question ("reset_answers")
 # - ability to have eight ball print all answers
 # - via easter egg question ("print_answers")
-require_relative 'answers'
+require 'colorize'
 
-puts 'Welcome to the Magic Eightball'
-puts 'Type quit when you\'re done'
+require_relative "answers.rb"
+require_relative "eightball_art.rb"
+puts "\n\n\n"
+puts `clear`
+space =' '*16
+b = "~*~".light_blue
+lines = "_"*33
+s = "*".light_green
+puts @art.blue
+puts
+print space
+puts lines.cyan
+print space
+print s, b, s, b, s, b, s, b, s, b, s, b, s, b, s, b, s
+puts
+print space
+puts ' Welcome to the Magic Eightball'.light_red
+print space
+puts '   Type quit when you\'re done'.magenta
+print space
+puts lines.cyan
+print space
+print s, b, s, b, s, b, s, b, s, b, s, b, s, b, s, b, s
+print space
+puts "\n\n"
 
 @finished = false
 
+@answers = Answers.new
+
 def program
-  @answers = Answers.new 
+  @answers = Answers.new
   until @finished == true
-    print "Ask the Magic Eightball a question\n>"
+    print "Ask the Magic Eightball a question\n\n".light_blue
     question = gets.chomp
     if question.include? 'quit'
+      puts `clear`
+      print "\n\n\n\nMagic Eightball is sad to see you leave.".light_blue
+      sleep 1
+      puts `clear`
       exit(0)
+    # elsif question.include? 'sudo'
+    #   question.slice! 'sudo'
+    #   @answers<< question
+    #   puts "Thanks, your input was added to avlaiable answers.".light_cyan
     else
       puts
       puts @answers.rand_answers
@@ -31,5 +64,4 @@ def program
   end
 end
 
-
-program 
+program
