e9ed0abe156d461ac4bb7a966bbaef1a2afb156a
Bobby Caya <bob.caya1@gmail.com>
2018-02-09 17:05:31 -0700

    Adding 8ball answers

--- answers.rb
+++ answers.rb
@@ -2,7 +2,7 @@ class Answers
   attr_accessor
 
   def initialize
-    @answers = ["Yes", "No", "Maybe","Outlook does not good", "Idk dude?", "It is a definite maybe", "Very doubtful", "You are really asking an 8 Ball for advice?", "It is certain", "Without a doubt", "As I see it, yes", "Outlook good", "Very doubtful", "Concentrate and ask again", "Very doubtful"]
+    @answers = ["Yes", "No", "Maybe","Outlook does not good", "Idk dude?", "It is a definite maybe", "Very doubtful", "You are really asking an 8 Ball for advice?", "It is certain", "Without a doubt", "As I see it, yes", "Outlook good", "Very doubtful", "Concentrate and ask again", "Very doubtful", "Hmm not sure, ask again"]
   end 
 
   def rand_answers
